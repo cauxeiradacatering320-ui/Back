b9bd16552afeb91ce83c8ab608b90b17f8f8c054
Pedro Luiz <pedrinholos11@gmail.com>
2026-05-24 20:47:08 -0300

    CORS

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ const app = fastify({
 
 // Registrar Plugins
 app.register(cors, {
-  origin: ["http://localhost:3000","https://front-cauxeirada.vercel.app"],
+  origin: ["http://localhost:3000","https://front-cauxeirada.vercel.app","https://front-ashy-ten-79.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
